90255b00c44ef511a81f5e4f039b4e6c477c6702
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2019-04-30 14:35:14 -0300

    Test NPM ONOFF

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var relay = new Gpio(26, 'out'); //use GPIO pin 4, and specify that it is output
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-    const customers = [
-        { id: 0, name: 'pepe' }
-    ];
-    res.json(customers);
+app.get('/api/turnOn', (req, res) => {
+    let ret = false;
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+        LED.writeSync(1); //set pin state to 1 (turn LED on)
+        ret = true;
+    } else {
+        LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+    res.json(ret);
 })
 
 const port = 5000;
